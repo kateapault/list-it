339466434fb5f0087d7c95d011df95bb3e6eaaed
github username <kateapault@gmail.com>
2020-05-14 12:23:07 -0400

    commented out NavBar in Header fso site is just alphabet die

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
     return(
         <div className="header">
             <div className="site-title">List-It</div>
-            <NavBar changePage={props.changePage}/>
+            {/* <NavBar changePage={props.changePage}/> */}
         </div>
     )
 };
