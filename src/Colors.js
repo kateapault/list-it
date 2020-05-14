7d8f976bc66c9b6f8c30eb3b33ec336b7c922391
github username <kateapault@gmail.com>
2020-05-14 12:22:06 -0400

    added color palette, created effect for alphabet die 'button' click to make it look like it's been pressed down

---
+++ src/Colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+    light: '#F1FFFA',
+    dark: '#454545',
+    primary: '#785964',
+    secondary: '#93B7BE',
+    accent: '#D5C7BC',
+}
+
+export default Colors
--- src/components/AlphabetDice.js
+++ src/components/AlphabetDice.js
@@ -3,15 +3,48 @@ import React from 'react'
 class AlphabetDice extends React.Component{
     state = {
         letter: 'A',
+        clickEffectActive: false,
+    }
+
+    loadingA = () => {
+        this.setState({ letter: '+'})
+    }
+
+    loadingB = () => {
+        this.setState({letter: 'x'})
+    }
+
+    depressButtonEffect = () => {
+        this.setState({
+            letter: '--',
+            clickEffectActive: true
+        })
+    }
+
+    buttonReleaseEffect = () => {
+        let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        this.setState({
+            letter: alphabet[Math.floor(Math.random()*26)],
+            clickEffectActive: false
+        })
     }
 
     rollDie = () => {
-        let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        this.setState({letter: alphabet[Math.floor(Math.random()*26)]})
+        this.depressButtonEffect()
+        setTimeout(this.buttonReleaseEffect,750)
     }
+
     render(){
+
+        let diceClass
+        if (this.state.clickEffectActive) {
+            diceClass = "dice-active"
+        } else {
+            diceClass = "dice"
+        }
+
         return (
-            <div onClick={this.rollDie} className="dice">
+            <div onClick={this.rollDie} className={diceClass}>
                 <div className="hexagon-front"/>
                 <div className="letter hexagon-middle">{this.state.letter}</div>
                 <div className="hexagon-back"/>
